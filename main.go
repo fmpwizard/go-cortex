f1c9c2728bd9941ed7d067a5c8d033d0b4b015bb
Diego Medina <diego@fmpwizard.com>
2014-03-28 22:50:55 -0400

    Removed the record service.
    Recording using the wolfram built in mic did not give good results. And
    voice recognition wasn't great either, so we removed the
    recording code to keep the project clean.
    
    Instead we will use sms to get commands

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"github.com/fmpwizard/go-cortex/services"
-	"log"
 	"net/http"
 )
 
@@ -17,31 +16,8 @@ func init() {
 
 func main() {
 	flag.Parse()
-	listenForCommands()
-
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(fmt.Sprintf(":%v", httpPort), nil)
-}
-
-//listenForCommands starts listening for the Arduino command
-// to start recording a voice command.
-//Once we get a voice command, the ARduino service will
-//send it to Wit and then the intent channel will get the
-// go struct with the information we need.
-//We then call ProcessIntent and start listening for
-//a new Arduino command.
-func listenForCommands() {
-	log.Println("1- listening for commands.")
-	intent := make(chan services.WitMessage)
-	go services.ArduinoIn(intent)
-	go func() {
-		select {
-		case ret := <-intent:
-			log.Printf("Got intent %+v\n", ret)
-			ProcessIntent(ret)
-			listenForCommands()
-		}
-	}()
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
